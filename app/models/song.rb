92be43a8b18660b80ae9c32136c2a7a561a19afb
ciyuxu <YOUR@EMAIL.com>
2017-09-07 23:47:54 +0200

    create artists overview/index page

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,6 @@
 class Song < ApplicationRecord
     belongs_to :artist
+
+    validate :name, presence: true
+    validate :album, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#songs
+song1 = Song.create! (track: "Creep", lyrics: "Creep", album: "Pablo Honey")
+song2 = Song.create! (track: "Paranoid Android", lyrics: "Paranoid Android", album: "OK coumputerCreepCreep")
+song4 = Song.create! (track: "May Nothing But Happiness Come Through Your Door", lyrics: "May Nothing But Happiness Come Through Your Door", album: "Come on Die Young")
+song5 = Song.create! (track: "Cody", lyrics: "Cody", album: "Come on Die Young")
+song6 = Song.create! (track: "The Sun Smells Too Loud", lyrics: "The Sun Smells Too Loud", album: "The Hawk Is Howling")
+song7 = Song.create! (track: "I'm Jim Morrison, I'm Dead", lyrics: "I'm Jim Morrison, I'm Dead", album: "The Hawk Is Howling")
+song8 = Song.create! (track: "A Song For Our Fathers", lyrics: "A Song For Our Fathers", album: "How Strange, Innocence")
+song9 = Song.create! (track: "Snow And Lights", lyrics: "Snow And Lights", album: "How Strange, Innocence")
+song10 = Song.create! (track: "The Only Moment We Were Alone", lyrics: "The Only Moment We Were Alone", album: "The Earth Is Not a Cold Dead Place")
+
+
+
+
+#artists
+artist1 = Artist.create! (name: "Radiohead", age: 32, bio: "Radiohead are an English rock band from Abingdon, Oxfordshire, formed in 1985.")
+artist2 = Artist.create! (name: "Mogwai", age: 22, bio: "Mogwai are a Scottish post-rock band, formed in 1995 in Glasgow. ")
+artist3 = Artist.create! (name: "Explosions in the Sky", age: 17, bio: "Explosions in the Sky are an American post-rock band from Texas.")
